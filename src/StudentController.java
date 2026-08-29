5864d285f1de3fe5036d65e39df54e7500b05f41
David-Quispe <riboy1616@gmail.com>
2026-08-29 17:26:42 -0500

    cambio en StudentController.java

--- src/StudentController.java
+++ src/StudentController.java
@@ -1,3 +1,24 @@
-public class StudentController{
+import java.util.ArrayList;
+import java.util.List;
 
+public class StudentController {
+
+    private List<String> students = new ArrayList<>();
+
+    public void addStudent(String name) {
+        students.add(name);
+        System.out.println("Estudiante agregado: " + name);
+    }
+
+    public void removeStudent(String name) {
+        students.remove(name);
+        System.out.println("Estudiante eliminado: " + name);
+    }
+
+    public void listStudents() {
+        System.out.println("Lista de estudiantes:");
+        for (String student : students) {
+            System.out.println("- " + student);
+        }
+    }
 }
